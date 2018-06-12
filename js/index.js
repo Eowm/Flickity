0d43123f3cf46f0c0e95f2b3bab498fab9a5d2f8
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-12 21:21:16 +0200

    Fixing Task 2 v8

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
-var templateList = document.getElementById('carousel-cell').innerHTML;
+//window.initMap = function() {
+//	var uluru = {lat: -25.363, lng: 131.044};
+//	var map = new google.maps.Map(document.getElementById('map'), {
+//			zoom: 4,
+//			center: uluru
+//		});
+//	var marker = new google.maps.Marker({
+//	position: uluru,
+//			map: map
+//		}); 
+//	}	
+//initMap();
 
 
-var listItems = '';
+(function() {
+	var templateList = document.getElementById('carousel-cell').innerHTML;
 
-var templateItem = document.getElementById('carousel-cell').innerHTML;
 
-    Mustache.parse(templateItem);
+	var listItems = '';
 
-for(var i = 0; i < carouselData.length; i++){
+	var templateItem = document.getElementById('carousel-cell').innerHTML;
+
+    	Mustache.parse(templateItem);
+
+	for(var i = 0; i < carouselData.length; i++){
 		console.log(carouselData);
-		listItems += Mustache.render(templateList, carouselData[i]);
+		listItems += Mustache.render(templateItem, carouselData[i]);
 	}
 
-	var fullProductList = Mustache.render(templateList, {products: listItems});
-
+	var fullProductList = Mustache.render(templateItem, {products: listItems});
+	var results = document.getElementById('results');
 	results.insertAdjacentHTML('beforeend', fullProductList);
 
-
+})();
 
 
 var elem = document.querySelector('.main-carousel');
@@ -45,4 +60,3 @@ restartButton.addEventListener('click', function( event ) {
  // progress = Math.max( 0, Math.min( 1, progress ) );
  // progressBar.style.width = progress * 100 + '%';
 //});
-
