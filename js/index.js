c99d566b79c64ca396dd56e931e8084adbeeba10
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-13 14:02:00 +0200

    fixing task 2 v1

--- js/index.js
+++ js/index.js
@@ -1,53 +1,47 @@
-
-
-
-(function() {
-	var templateList = document.getElementById('carousel-cell').innerHTML;
-
-
-	var listItems = '';
-
-	var templateItem = document.getElementById('carousel-cell').innerHTML;
-
-    	Mustache.parse(templateItem);
-
-	for(var i = 0; i < carouselData.length; i++){
-		console.log(carouselData);
-		listItems += Mustache.render(templateItem, carouselData[i]);
-	}
-
-	var fullProductList = Mustache.render(templateItem, {products: listItems});
-	var results = document.getElementById('results');
-	results.insertAdjacentHTML('beforeend', fullProductList);
-
-})();
-var elem = document.querySelector('#results');	
-
-
-
-var flkty = new Flickity( elem, {
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-});
-
-
-
-var restartButton = document.querySelector('.restart-button');
-
-
-restartButton.addEventListener('click', function( event ) {
-  flkty.selectCell(0);
-});
-
+ar restartButton = document.querySelector('.restart');
 var progressBar = document.querySelector('.progress-bar')
+var templateSlide = document.getElementById('template-slide').innerHTML;
+var carouselContainer = document.querySelector('.carousel');
+var slide = ''
+
+restartButton.addEventListener( 'click', function() {
+  flkty.selectCell('#tempelhof');
+});
+
+Mustache.parse(templateSlide);
+
+for(var i = 0; i < carouselData.length; i++) {
+  slide = Mustache.render(templateSlide, carouselData[i]);
+  carouselContainer.insertAdjacentHTML('beforeend', slide);
+}
+
+var flkty = new Flickity( '.carousel', {
+  pageDots: false,
+  hash: true,
+  draggable: false,
+  cellSelector: ".carousel-cell"
+});
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 
 /*window.initMap = function() {
-	var places = {uluru: (lat: -25.363, lng: 131.044), coords2: (lat: -25.363, lng: 134.044), coords3: (lat: -25.363, lng: 137.044)};
+	var places = {
+  uluru: {
+    lat: -25.363, 
+    lng: 131.044
+   },
+  coords2: {
+    lat: -25.363, 
+    lng: 134.044
+  },
+  coords3: {
+    lat: -25.363, 
+    lng: 137.044
+  }
+};
 	
 	var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 6,
@@ -89,8 +83,3 @@ var progressBar = document.querySelector('.progress-bar')
 		});	 
 	}	
 initMap();*/
-
-//.on( 'scroll', function( progress ) {
- // progress = Math.max( 0, Math.min( 1, progress ) );
- // progressBar.style.width = progress * 100 + '%';
-//});
